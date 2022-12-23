4b7cf102d77b873c7fa13bdc63ad7e7574d2ab70
Robert McKinnon <robert.mckinnon@astrazeneca.com>
2022-12-23 12:52:04 -0500

    added formula for az-react-node

---
+++ az-react-node.rb
@@ -0,0 +1,11 @@
+class AzReactNode < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/robertaz1/az-react-node/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "71025815d585ff6929feac76acb64531b01b60347c0843ddb6d7779189165c1f"
+  license ""
+
+  def install
+    bin.install "az-react-node"
+  end
+end
